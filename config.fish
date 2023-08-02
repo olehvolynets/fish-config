2bc8ae3ef3df64608ef1672dd88bddb008983172
Oleh Volynets <saggot91@gmail.com>
2023-08-02 14:31:17 +0200

    Accumulated changes

--- config.fish
+++ config.fish
@@ -1,11 +1,17 @@
 if status is-interactive
   set -x EDITOR nvim
   set -x VISUAL nvim
+  set -a PATH ./scripts
 
   alias ll="exa -lah --icons --group-directories-first"
-  alias vim="nvim"
-  alias vi="nvim"
-  alias v="nvim"
+  # alias vim="nvim"
+  # alias vi="nvim"
+  # alias v="nvim"
+  alias vim="echo \"use nya, bakka ฅ(ΦωΦ)ฅ\""
+  alias vi="echo \"use nya, bakka ฅ(ΦωΦ)ฅ\""
+  alias v="echo \"use nya, bakka ฅ(ΦωΦ)ฅ\""
+  alias nya="nvim"
+  alias nyan="nvim"
 
   starship init fish | source
 
@@ -13,6 +19,11 @@ if status is-interactive
 end
 
 function fish_greeting
+end
+
+set -l env_dir_path ~/.config/fish/env
+for env_file in (ls $env_dir_path)
+    source $env_dir_path/$env_file
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
